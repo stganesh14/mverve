480dd7cfd5d7447bc936ffd286be641cb402d7bd
stganesh14 <86861152+stganesh14@users.noreply.github.com>
2022-04-20 10:28:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,4 +9,22 @@ function myFunction() {
       x.type = "password";
     }
 }
-
+function myFunction2() {
+  var x = document.getElementById("cpassword");
+  if (x.type === "password")
+   {
+    x.type = "text";
+  } 
+  else 
+  {
+    x.type = "password";
+  }
+}
+function validateForm() {
+  let a = document.forms["myForm"]["pass"].value;
+  let b = document.forms["myForm"]["cpass"].value;
+  if (a != b) {
+    alert("password and confirm must be same");
+    return false;
+  }
+}
